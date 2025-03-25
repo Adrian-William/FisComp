798ea0708b7483401ae406cc93acd74882bb8cc9
Adrian William lab <adrianwilliam@ufu.br>
2025-03-25 16:08:23 -0300

    grafico animado

--- Lista 3/caos.py
+++ Lista 3/caos.py
@@ -7,8 +7,9 @@ file_path = os.path.join(os.path.dirname(__file__), 'caos.txt')
 y, x = np.loadtxt(file_path)
 color = np.ones(len(x))
 
-
+print(len(x))
+print(len(y))
 plt.ion()
-plt.scatter(x,y,s=0.01,c=color,alpha=1)
+plt.scatter(x,y,s=0.01,c='Black',alpha=1)
 plt.show()
-plt.pause(10)
+plt.pause(100)
--- Lista 3/teste.py
+++ Lista 3/teste.py
@@ -3,24 +3,31 @@ import numpy as np
 import os
     
 
-# Fixing random state for reproducibility
 path = os.path.join(os.path.dirname(__file__), 'caos.txt')
 y, x = np.loadtxt(path)
 
-figzoom, axzoom = plt.subplots(figsize=(5,5))
+figzoom, axzoom = plt.subplots(figsize=(8,8))
 
 axzoom.set( autoscale_on=False, title='Zoom window')
 
 
-axzoom.scatter(x, y, s=0.01, c='b', alpha=1)
+ang = [2.45, 0.26, 0.849, 0.066]
+cons = [1, 4, 0, 1]
+for j in range(100):
 
+    for i in range(100):
 
-for i in range(100):
-#    sleep(0.1)
-    axzoom.set_xlim(i - 0.1, i + 0.1)
-    axzoom.set_ylim(i - 0.1, i + 0.1)
+        a=i*0.01
+        alpha = 0.5 + 0.5*a
+        axzoom.clear()
+        axzoom.scatter(x, y, s=0.1, edgecolors='Black', alpha=alpha)
+
+        
+        axzoom.set_xlim(a*ang[0]+cons[0] , cons[1]-a*ang[1])
+        axzoom.set_ylim(a*ang[2]+cons[2] , cons[3]-a*ang[3])
+        figzoom.canvas.draw()
+        plt.pause(0.0001)
+    ang = [0.6, 0.12, 0.17, 0.03]
+    cons = [3, 3.843, 0.66, 0.956]
     
-    figzoom.canvas.draw()
-
-figzoom.canvas.mpl_connect('button_press_event', on_press)
 plt.show()
