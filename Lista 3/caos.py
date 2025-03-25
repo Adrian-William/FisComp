f00340a7f02086f87f04f2b9cd10a426b9f6dfac
xablau <adrianwilliam78@gmail.com>
2025-03-25 17:34:21 -0300

    aula alguma coisa

--- Lista 3/caos.py
+++ Lista 3/caos.py
@@ -3,13 +3,16 @@ import matplotlib.pyplot as plt
 
 import os
 
-file_path = os.path.join(os.path.dirname(__file__), 'caos.txt')
-y, x = np.loadtxt(file_path)
+file_path = os.path.join(os.path.dirname(__file__), 'caos.npz')
+data = np.load(file_path)
+x = data['x']
+y = data['y']
+
 color = np.ones(len(x))
 
 print(len(x))
 print(len(y))
 plt.ion()
-plt.scatter(x,y,s=0.01,c='Black',alpha=1)
-plt.show()
-plt.pause(100)
+plt.scatter(x,y,s=0.01,c='Black',alpha=0.05)
+plt.show(block=True)
+
--- Lista 3/teste.py
+++ Lista 3/teste.py
@@ -1,33 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import time
     
-
-path = os.path.join(os.path.dirname(__file__), 'caos.txt')
-y, x = np.loadtxt(path)
-
+file_path = os.path.join(os.path.dirname(__file__), 'caos.npz')
+data = np.load(file_path)
+x = data['x']
+y = data['y']
 figzoom, axzoom = plt.subplots(figsize=(8,8))
 
-axzoom.set( autoscale_on=False, title='Zoom window')
+axzoom.set( autoscale_on=True, title='Zoom window')
 
 
-ang = [2.45, 0.26, 0.849, 0.066]
+ang = [2.472, 0.37, 0.8573, 0.0948]
 cons = [1, 4, 0, 1]
-for j in range(100):
+init = [100,0.01]
+t = lt =0
+fps = 1/30
+for j in range(10):
 
-    for i in range(100):
-
-        a=i*0.01
-        alpha = 0.5 + 0.5*a
+    for i in range(init[0]):
+        t = time.thread_time()
+        daltat = t-lt
+        lt = t
+        print(daltat)
+        a=i*init[1]
+        alpha = 0.1 + 0.5*a
         axzoom.clear()
-        axzoom.scatter(x, y, s=0.1, edgecolors='Black', alpha=alpha)
-
-        
+        axzoom.scatter(x, y, s=0.1, edgecolors='Black', alpha=alpha)       
         axzoom.set_xlim(a*ang[0]+cons[0] , cons[1]-a*ang[1])
         axzoom.set_ylim(a*ang[2]+cons[2] , cons[3]-a*ang[3])
         figzoom.canvas.draw()
-        plt.pause(0.0001)
-    ang = [0.6, 0.12, 0.17, 0.03]
-    cons = [3, 3.843, 0.66, 0.956]
-    
+        if daltat < fps:
+            frame = fps-daltat
+        else:
+            frame = fps
+        plt.pause(fps)
+    ang = [0.45, 0.24, 0.17, 0.03]
+    cons = [3, 3.843, 0.66, 0.96]
+    init = [50,0.02]
+plt.show() 
+'''
+axzoom.scatter(x, y, s=0.1, edgecolors='Black', alpha=0.1)  
+for i in range(init[0]):
+    a=i*init[1]
+    axzoom.set_xlim(a*ang[0]+cons[0] , cons[1]-a*ang[1])
+    axzoom.set_ylim(a*ang[2]+cons[2] , cons[3]-a*ang[3])
+    figzoom.canvas.draw()
+    plt.pause(0.1)
 plt.show()
+'''
